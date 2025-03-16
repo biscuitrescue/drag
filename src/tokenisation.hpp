892f606d8caf77fb59af2a605f4c30a9fc281279
biscuitrescue <karttikeyasinha11@gmail.com>
2025-03-16 17:39:19 +0530

    tokeniser

--- src/tokenisation.hpp
+++ src/tokenisation.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cctype>
+#include <cstdlib>
 #include <cwctype>
 #include <iostream>
 #include <optional>
@@ -21,58 +23,68 @@ struct Token {
 
 class Tokeniser {
   private:
+
+    [[nodiscard]] std::optional<char> peak(int n = 1) const { // nodiscard to warn if not using ret val because no reason not to use ret val from const function
+      if (m_ind + n >= m_src.length()) {
+        return {};
+      } else {
+        return m_src.at(m_ind);
+      }
+    }
+
+    char consume() {
+      return m_src.at(m_ind++);
+    }
+
     const std::string m_src;
+    int m_ind = 0;
+
   public:
-    inline Tokeniser(const std::string& src)
+
+    inline explicit Tokeniser(const std::string& src)
       : m_src(std::move(src)) {
 
       }
+
     inline std::vector<Token> tokenise() {
       std::vector<Token> token_array;
 
       std::string buf;
-
-      for (int i = 0; i < str.length(); i++) {
-        char c = str.at(i);
-        if (std::isalpha(c)) {
-          buf.push_back(c);
-          i++;
-          while (i < str.length() && std::isalnum(str.at(i))) {
-            buf.push_back(str.at(i));
-            i++;
+      while(peak().has_value()) {
+        if (isalpha(peak().value())) {
+          buf.push_back(consume());
+          while (peak().has_value() && std::isalnum(peak().value())) {
+            buf.push_back(consume());
           }
-          i--;
-
           if (buf == "exit") {
             token_array.push_back({.type = TokenType::exit});
             buf.clear();
             continue;
           } else {
-            // std::cerr << "Error ret" << buf << c << std::endl;
             std::cerr << "Unexpected identifier: " << buf << std::endl;
             exit(EXIT_FAILURE);
           }
-        } else if (std::iswspace(c)) {
-          // buf.clear();
+        } else if (peak().has_value() && std::iswspace(peak().value())) {
           continue;
-        } else if (std::isdigit(c)) {
-          buf.push_back(c);
-          i++;
-          while(i < str.length() && std::isdigit(str.at(i))) { // Shouldnt be i???? -> -> // was right
-            c = str.at(i);
-            buf.push_back(c);
-            i++;
+        } else if (std::isdigit(peak().value())) {
+          buf.push_back(consume());
+          while(peak().has_value() && std::isdigit(peak().value())) {
+            buf.push_back(consume());
           }
-          i--;
-          token_array.push_back({.type = TokenType::int_lit, .value = buf});
+          token_array.push_back({.type = TokenType::int_lit});
           buf.clear();
-        } else if (c == ';') {
+          continue;
+        } else if (peak().value() == ';') {
           token_array.push_back({.type = TokenType::semicol});
+          buf.clear();
+          continue;
         } else {
-          std::cerr << "Error dig" << std::endl;
+          std::cerr << "Error tokenising" << std::endl;
           exit(EXIT_FAILURE);
         }
-      }
+      } 
+
+      m_ind = 0;
       return token_array;
 
     };
