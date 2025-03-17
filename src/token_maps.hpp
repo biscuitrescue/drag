5b0a23ec628f3473f6c2471ad1c5a3317df2cc08
biscuitrescue <karttikeyasinha11@gmail.com>
2025-03-17 23:41:29 +0530

    cleaned up comments

--- src/token_maps.hpp
+++ src/token_maps.hpp
@@ -87,16 +87,7 @@ class Tokeniser {
           buf.clear();
           continue;
         }
-        // else if (peak().has_value()) {
-        //
-        //   buf.push_back(consume());
-        //   while(peak().has_value() && std::isdigit(peak().value())) {
-        //     buf.push_back(consume());
-        //   }
-        //   token_array.push_back({.type = TokenType::int_lit, .value = buf});
-        //   buf.clear();
-        //   continue;
-        // } 
+
         else if (peak().value() == ';') {
           consume();
           token_array.push_back({.type = TokenType::semicol, .value = ";"});
