a43586db4d859b7401d265864d13592a9ea9e3c0
biscuitrescue <karttikeyasinha11@gmail.com>
2025-03-18 20:27:32 +0530

    parsing checkpt

--- src/parser.hpp
+++ src/parser.hpp
@@ -40,19 +40,31 @@ class Parser {
     inline explicit Parser(std::vector<Token> token_array):
       m_tokens(std::move(token_array)) {}
 
-    std::optional<node::Exit> parse_expr() { }
+// Expression Parser
+    std::optional<node::Expr> parse_expr() { 
+      if (peak().has_value() && peak().value().type == TokenType::int_lit) {
+        return node::Expr{.int_lit = consume()};
+      } else {
+        return {};
+      }
+    }
 
+// Exit Parser
     std::optional<node::Exit> parse() {
       std::optional<node::Exit> node_exit;
       while (peak().has_value()) {
         if (peak().value().type == TokenType::exit) {
-          if (auto expr = parse_expr()) {
-
+          consume();
+          if (auto expr = parse_expr()) { // if optional has val -> true else false
+            node_exit = node::Exit{.expr = expr.value()};
           } else {
             std::cerr << "invalid expr" << std::endl;
             exit(EXIT_FAILURE);
           }
-
+          if (!peak().has_value() || peak().value().type != TokenType::semicol) {
+            std::cerr << "invalid expr" << std::endl;
+            exit(EXIT_FAILURE);
+          }
         }
       }
       m_ind = 0;
