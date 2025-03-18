6dfa887859b26c9f08f343080bea9a952f72235f
biscuitrescue <karttikeyasinha11@gmail.com>
2025-03-18 21:20:25 +0530

    added generator

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,17 @@
+#include <cstdlib>
 #include <fstream>
+#include <optional>
 #include <ostream>
 #include <sstream>
 #include <string>
+#include <utility>
 #include <vector>
 #include <iostream>
 
 #include "./token_maps.hpp"
-// #include "./parser.hpp"
+#include "./generation.hpp"
+#include "./parser.hpp"
 
-
-std::string make_asm(const std::vector<Token> &token_array) { // err here
-  std::stringstream output;
-  // init global line and status for sstream
-  output << "global _start\n_start:\n";
-
-  for (int i = 0; i < token_array.size(); i++) {
-    const Token& token = token_array.at(i);
-      if (token.type == TokenType::exit) {
-        if (i + 1 < token_array.size() && token_array.at(i + 1).type == TokenType::int_lit) {
-          if (i + 2 < token_array.size() && token_array.at(i + 2).type == TokenType::semicol) {
-            // std::cout << "Generating asm: " << token_array.at(i + 1).value.value() << std::endl;
-
-            output << "    mov rax, 60\n";
-            output << "    mov rdi, " << token_array.at(i + 1).value.value() << "\n";
-            output << "    syscall";// terminate
-            
-            i += 2;
-          } else {
-            std::cerr << "Error: Expected semicolon after integer literal in exit statement\n";
-            exit(EXIT_FAILURE);
-          }
-        } else {
-          std::cerr << "Error: Expected integer literal after exit\n";
-          exit(EXIT_FAILURE);
-        }
-      }
-  }
-
-  return output.str();
-}
 
 int main(int argc, char* argv[]) {
   if (argc != 2) {
@@ -58,11 +31,19 @@ int main(int argc, char* argv[]) {
   Tokeniser tokeniser(std::move(contents));
   std::vector<Token> token_array = tokeniser.tokenise();
 
-  // std::cout << make_asm(token_array) << std::endl;
+  Parser parser(std::move(token_array));
+  std::optional<node::Exit> tree = parser.parse();
+
+  if (!tree.has_value()) {
+    std::cerr << "No exit statement" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  Generator generator(tree.value());
 
   {
     std::ofstream file("./out.asm");
-    file << make_asm(token_array);
+    file << generator.generate();
   }
 
   system("nasm -felf64 out.asm");
