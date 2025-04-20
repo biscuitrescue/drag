90f7d7a3d56e60570b400cf9dafadd51194a3b79
Karttikeya Sinha <biscuitrescue>
2025-04-21 01:43:40 +0530

    updates

--- src/parser.hpp
+++ src/parser.hpp
@@ -25,105 +25,121 @@ namespace node {
 
   struct StmtExit {
     node::Expr expr;
-  };
+};
 
-  struct StmtLet {
-    Token ident;
-    node::Expr expr;
-  };
+struct StmtLet {
+  Token ident;
+  node::Expr expr;
+};
 
-  struct Stmt {
-    std::variant<StmtExit, StmtLet> var;
-  };
+struct Stmt {
+  std::variant<StmtExit, StmtLet> var;
+};
 
-  struct Prog {
-    std::vector<Stmt> stmts;
-  };
+struct Prog {
+  std::vector<Stmt> stmts;
+};
 } // namespace node
 
 class Parser {
-  private:
-    const std::vector<Token> m_tokens;
-    size_t m_ind = 0;
+private:
+  const std::vector<Token> m_tokens;
+  size_t m_ind = 0;
 
-    [[nodiscard]] inline std::optional<Token> peak(int offset = 0)
-      const { // nodiscard to warn if not using ret val because no reason not to
-              // use ret val from const function
-        if (m_ind + offset >= m_tokens.size()) {
-          return {};
-        }
-        return m_tokens.at(m_ind + offset);
-      }
+  [[nodiscard]] inline std::optional<Token> peak(int offset = 0)
+  const { // nodiscard to warn if not using ret val because no reason not to
+    // use ret val from const function
+    if (m_ind + offset >= m_tokens.size()) {
+      return {};
+    }
+    return m_tokens.at(m_ind + offset);
+  }
 
-    inline Token consume() { return m_tokens.at(m_ind++); }
+  inline Token consume() { return m_tokens.at(m_ind++); }
 
-  public:
-    inline explicit Parser(std::vector<Token> token_arr)
-      : m_tokens(std::move(token_arr)) {}
+public:
+  inline explicit Parser(std::vector<Token> token_arr)
+  : m_tokens(std::move(token_arr)) {}
 
-    std::optional<node::Expr> parse_expr() {
-      if (peak().has_value() && peak()->type == TokenType::int_lit) {
-        return node::Expr{.var = node::ExprIntLit{.int_lit = consume()}};
-      } else if (peak().has_value() && peak()->type == TokenType::ident) {
-        return node::Expr{.var = node::ExprIdent{.ident = consume()}};
+  std::optional<node::Expr> parse_expr() {
+    if (peak().has_value() && peak()->type == TokenType::int_lit) {
+      return node::Expr{.var = node::ExprIntLit{.int_lit = consume()}};
+    } else if (peak().has_value() && peak()->type == TokenType::ident) {
+      return node::Expr{.var = node::ExprIdent{.ident = consume()}};
+    } else {
+      return {};
+    }
+  }
+
+  std::optional<node::Stmt> parse_stmt() {
+    if (peak().value().type == TokenType::exit && peak(1).has_value() &&
+      peak(1)->type == TokenType::open_paren) {
+
+      consume();
+      consume();
+
+      node::StmtExit stmt_exit;
+
+      // Evaluate exit expr 
+      if (auto node_expr = parse_expr()) {
+        stmt_exit = node::StmtExit{.expr = node_expr.value()};
       } else {
-        return {};
+        std::cerr << "Invalid expression" << std::endl;
+        exit(EXIT_FAILURE);
+      }
+
+      // check for )
+      if (peak().has_value() && peak()->type == TokenType::close_paren) {
+        consume();
+      } else {
+        std::cerr << "Expected ')'" << std::endl;
+        exit(EXIT_FAILURE);
+      }
+
+      // check for semicol
+      if (peak().has_value() && peak().value().type == TokenType::semicol) {
+        consume();
+      } else {
+        std::cerr << "Expected ';'" << std::endl;
+        exit(EXIT_FAILURE);
+      }
+
+      return node::Stmt{.var = stmt_exit};
+
+    } else if (peak().has_value() && peak()->type == TokenType::let && peak(1).has_value() && peak(1)->type == TokenType::ident && peak(2).has_value() && peak(2)->type == TokenType::assign) { // let parser
+      consume();
+
+      auto stmt_let = node::StmtLet {.ident = consume()};
+
+      consume();
+
+      if (auto expr = parse_expr()) {
+        stmt_let.expr = expr.value();
+      } else {
+        std::cerr << "Invalid expression in let stmt" << std::endl;
+        exit(EXIT_FAILURE);
+      }
+
+      if (peak().has_value() && peak()->type == TokenType::semicol) {
+        consume();
+      } else {
+        std::cerr << "Expected ';' at end of statement let" << std::endl;
+        exit(EXIT_FAILURE);
+      }
+      return node::Stmt {.var = stmt_let};
+    } else { return {}; }
+  }
+
+  // program parser
+  std::optional<node::Prog> parse_prog() {
+    node::Prog prog;
+    while (peak().has_value()) {
+      if (auto stmt = parse_stmt()) {
+        prog.stmts.push_back(stmt.value());
+      } else {
+        std::cerr << "Invalid stmt" << std::endl;
+        exit(EXIT_FAILURE);
       }
     }
-
-    std::optional<node::Stmt> parse_stmt() {
-      if (peak().value().type == TokenType::exit && peak(1).has_value() &&
-          peak(1)->type == TokenType::open_paren) {
-
-        consume();
-        consume();
-
-        node::StmtExit stmt_exit;
-
-        // Evaluate expr
-        if (auto node_expr = parse_expr()) {
-          stmt_exit = node::StmtExit{.expr = node_expr.value()};
-        } else {
-          std::cerr << "Invalid expression" << std::endl;
-          exit(EXIT_FAILURE);
-        }
-
-        // check for )
-        if (peak().has_value() && peak()->type == TokenType::close_paren) {
-          consume();
-        } else {
-          std::cerr << "Expected ')'" << std::endl;
-          exit(EXIT_FAILURE);
-        }
-
-        // check for semicol
-        if (peak().has_value() && peak().value().type == TokenType::semicol) {
-          consume();
-        } else {
-          std::cerr << "Expected ';'" << std::endl;
-          exit(EXIT_FAILURE);
-        }
-
-        return node::Stmt{.var = stmt_exit};
-
-      } else if (peak().has_value() && peak()->type == TokenType::let && peak(1).has_value() && peak(1)->type == TokenType::ident && peak(2).has_value() && peak(2)->type == TokenType::assign) {
-        consume();
-        auto stmt_let = node::StmtLet {.ident = consume()};
-        consume();
-
-        if (auto expr = parse_expr()) {
-          stmt_let.expr = expr.value();
-        } else {
-          std::cerr << "Invalid expression in let stmt" << std::endl;
-          exit(EXIT_FAILURE);
-        }
-
-        if (peak().has_value() && peak()->type == TokenType::semicol) {
-          consume();
-        } else {
-          std::cerr << "Expected ';' at end of statement let" << std::endl;
-          exit(EXIT_FAILURE);
-        }
-      }
-    }
+  }
 };
